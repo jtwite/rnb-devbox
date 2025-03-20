32a8ff1701570692eca6b2359cead656511dfd96
Jake Twite <jtwite@roomandboard.com>
2025-03-20 14:06:11 -0500

    Fixing things

--- setup-env.sh
+++ setup-env.sh
@@ -10,10 +10,8 @@ if [[ -z "${NIX_JAVA_DIR}" ]]; then
 elif [[ -z "$NIX_JAVA_DIR" ]]; then
 	NIX_JAVA_DIR="/nix/store/$(ls /nix/store/ | grep -e ".*temurin.*[0-9\.]$" | tail -1)"
 fi
-
+rsync -a --chmod=ugo=rwX --ignore-existing $NIX_JAVA_DIR/* $userJavaDir
 echo " - devbox java is installed here: $NIX_JAVA_DIR"
-
-rsync -a --chmod=ugo=rwX --update $NIX_JAVA_DIR/* $userJavaDir
 
 profileJavaHomeCount=$(cat ~/.profile | grep "JAVA_HOME" | wc -l | sed 's/[[:space:]]*//g')
 bashrcJavaHomeCount=$(cat ~/.bashrc | grep "JAVA_HOME" | wc -l | sed 's/[[:space:]]*//g')
